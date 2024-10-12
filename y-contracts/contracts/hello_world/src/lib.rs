c694fec0e9fec5b054a059579c9bec2441046afd
jacksmithinsulander <666.jack.smith@protonmail.com>
2024-10-12 16:31:50 +0100

    fixes and deploy

--- y-contracts/contracts/hello_world/src/lib.rs
+++ y-contracts/contracts/hello_world/src/lib.rs
@@ -3,8 +3,6 @@ use core::fmt::{Debug, Formatter};
 
 use soroban_sdk::{contract, contractimpl, contracttype, crypto, symbol_short, vec, Address, BytesN, Env, FromVal, String, Symbol, Vec};
 
-//const YEETS: Symbol = symbol_short!("YEETS");
-
 #[contract]
 pub struct YContract;
 
@@ -17,7 +15,6 @@ pub enum YeetKey {
 pub struct Yeet {
     message: String,
     author: Address,
-    yeet_id: String,
     likes: u64,
     replies: Vec<Yeet>
 }
@@ -32,7 +29,6 @@ impl YContract {
         env.storage().temporary().set(&yeet_key, &Yeet {
             message: message,
             author: user.clone(),
-            yeet_id: id,
             likes: 0,
             replies: Vec::new(&env)
         });
@@ -40,13 +36,49 @@ impl YContract {
         env.storage().temporary().extend_ttl(&yeet_key, initial_validity, initial_validity);
     }
 
-    pub fn reply(env: Env, yeet_id: String) {}
+    pub fn reply(env: Env, user: Address, reply: String, id: String, added_validity: u32) {
+        user.require_auth();
 
-    pub fn sheesh(env: Env, yeet_id: String) {}
+        let yeet_key = YeetKey::Of(id.clone());
 
-    pub fn get_yeet(env: Env, yeet_id: String) {}
+        let mut root_yeet: Yeet = env.storage().temporary().get(&yeet_key).expect("This fucking yeet doesnt exist");
 
-    pub fn get_yeets_by_address(env: Env, who: &Address) {}
+        let reply_yeet: Vec<Yeet> = vec![&env, Yeet {
+            message: reply,
+            author: user.clone(),
+            likes: 0,
+            replies: Vec::new(&env),
+        }];
+
+        root_yeet.replies.append(&reply_yeet);
+
+        env.storage().temporary().set(&yeet_key, &root_yeet);
+
+        env.storage().temporary().extend_ttl(&yeet_key, added_validity, added_validity);
+
+    }
+
+    pub fn sheesh(env: Env, user: Address, id: String, added_validity: u32) {
+        user.require_auth();
+
+        let yeet_key = YeetKey::Of(id.clone());
+
+        let mut root_yeet: Yeet = env.storage().temporary().get(&yeet_key).expect("This fucking yeet doesnt exist");
+
+        root_yeet.likes += 1;
+
+        env.storage().temporary().set(&yeet_key, &root_yeet);
+
+        env.storage().temporary().extend_ttl(&yeet_key, added_validity, added_validity);
+    }
+
+    pub fn get_yeet(env: Env, id: String) -> Yeet {
+        let yeet_key = YeetKey::Of(id.clone());
+
+        env.storage().temporary().get(&yeet_key).expect("This fucking yeet doesnt exist")
+    }
+
+    // pub fn get_yeets_by_address(env: Env, who: &Address) {}
 
 }
 
