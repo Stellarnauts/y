46b4a0283450a077ae6e30759799b7374e0d8577
jacksmithinsulander <666.jack.smith@protonmail.com>
2024-10-12 18:11:42 +0100

    more tests

--- y-contracts/contracts/y/src/test.rs
+++ y-contracts/contracts/y/src/test.rs
@@ -32,8 +32,8 @@ fn test_yeet() {
                 contract_id.clone(),
                 (YEET, symbol_short!("yeet")).into_val(&env),
                 Yeet {
-                    message: message,
-                    author: user_1,
+                    message: message.clone(),
+                    author: user_1.clone(),
                     likes: 0,
                     replies: Vec::new(&env)
                 }.into_val(&env)
@@ -51,7 +51,7 @@ fn test_yeet() {
                 Yeet {
                     message: message,
                     author: user_1,
-                    likes: 0,
+                    likes: 1,
                     replies: Vec::new(&env)
                 }.into_val(&env)
             ),
@@ -59,5 +59,72 @@ fn test_yeet() {
     );
 }
 
-// #[test]
-// fn test_
+#[test]
+fn test_sheesh() {
+    let env = Env::default();
+    env.mock_all_auths();
+
+    let contract_id = env.register_contract(None, YContract);
+    let client = YContractClient::new(&env, &contract_id);
+
+    let user_1 = Address::generate(&env);
+    let user_2 = Address::generate(&env);
+
+    let message = String::from_str(&env, "Heeej");
+    let id = String::from_str(&env, "kdjflksd9399kjf");
+
+    client.yeet(&user_1, &message, &id, &3000);
+
+    assert_eq!(
+        env.events().all(),
+        vec![
+            &env,
+            (
+                contract_id.clone(),
+                (YEET, symbol_short!("yeet")).into_val(&env),
+                Yeet {
+                    message: message.clone(),
+                    author: user_1.clone(),
+                    likes: 0,
+                    replies: Vec::new(&env)
+                }.into_val(&env)
+            ),
+        ]
+    );
+
+    assert_ne!(
+        env.events().all(),
+        vec![
+            &env,
+            (
+                contract_id.clone(),
+                (YEET, symbol_short!("yeet")).into_val(&env),
+                Yeet {
+                    message: message.clone(),
+                    author: user_1.clone(),
+                    likes: 1,
+                    replies: Vec::new(&env)
+                }.into_val(&env)
+            ),
+        ]
+    );
+
+    client.sheesh(&user_2, &id, &3000);
+
+    assert_eq!(
+        env.events().all(),
+        vec![
+            &env,
+            (
+                contract_id.clone(),
+                (YEET, symbol_short!("yeet")).into_val(&env),
+                Yeet {
+                    message: message,
+                    author: user_1,
+                    likes: 1,
+                    replies: Vec::new(&env)
+                }.into_val(&env)
+            ),
+        ]
+    );
+}
