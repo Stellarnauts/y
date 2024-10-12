f9cddd563f25a739fe4e42b69c7dba8754b300ea
jacksmithinsulander <666.jack.smith@protonmail.com>
2024-10-12 15:52:30 +0100

    yeet implemented

--- y-contracts/contracts/hello_world/src/lib.rs
+++ y-contracts/contracts/hello_world/src/lib.rs
@@ -1,13 +1,21 @@
 #![no_std]
-use soroban_sdk::{contract, contractimpl, symbol_short, vec, Env, String, Symbol, Vec, Address};
+use core::fmt::{Debug, Formatter};
 
-const YEETS: Symbol = symbol_short!("YEETS");
+use soroban_sdk::{contract, contractimpl, contracttype, crypto, symbol_short, vec, Address, BytesN, Env, FromVal, String, Symbol, Vec};
+
+//const YEETS: Symbol = symbol_short!("YEETS");
 
 #[contract]
 pub struct YContract;
 
-struct Yeet {
-    uri: String,
+#[contracttype]
+pub enum YeetKey {
+    Of(String)
+}
+
+#[contracttype]
+pub struct Yeet {
+    message: String,
     author: Address,
     yeet_id: String,
     likes: u64,
@@ -16,13 +24,30 @@ struct Yeet {
 
 #[contractimpl]
 impl YContract {
-    pub fn yeet(env: Env, yeet: Yeet) {
-        
+    pub fn yeet(env: Env, user: Address, message: String, id: String, initial_validity: u32) {
+        user.require_auth();
+
+        let yeet_key = YeetKey::Of(id.clone());
+
+        env.storage().temporary().set(&yeet_key, &Yeet {
+            message: message,
+            author: user.clone(),
+            yeet_id: id,
+            likes: 0,
+            replies: Vec::new(&env)
+        });
+
+        env.storage().temporary().extend_ttl(&yeet_key, initial_validity, initial_validity);
     }
 
     pub fn reply(env: Env, yeet_id: String) {}
 
     pub fn sheesh(env: Env, yeet_id: String) {}
+
+    pub fn get_yeet(env: Env, yeet_id: String) {}
+
+    pub fn get_yeets_by_address(env: Env, who: &Address) {}
+
 }
 
 mod test;
