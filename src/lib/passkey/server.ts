f0a694240d04c2a02f5a9122675b744f20c2e4cc
Gustav Genberg <gurra.genberg@gmail.com>
2024-10-13 11:30:10 +0200

    fix: launchtube

--- src/lib/passkey/server.ts
+++ src/lib/passkey/server.ts
@@ -3,9 +3,8 @@ import { PasskeyServer } from "passkey-kit";
 
 export const server = new PasskeyServer({
   rpcUrl: config.stellar.rpcUrl,
-  launchtubeUrl: "https://testnet.launchtube.xyz",
-  launchtubeJwt:
-    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJjM2RjYzBkZTE0YzQxOGY3NTc1ZGIyNmEzNTQ4NzI5ZmZkNTM4YTkyNjZlNDFmYzU1MDM3Yjc3MTg2MTM2OWRiIiwiZXhwIjoxNzM2MDE0OTE2LCJjcmVkaXRzIjoxMDAwMDAwMDAwLCJpYXQiOjE3Mjg3NTczMTZ9.o5TYN0r1g7cZg3ylnMmAiCxF4tIq2oljHVB8LSwh7iw",
+  launchtubeUrl: config.launchtube.url,
+  launchtubeJwt: config.launchtube.jwt,
   // mercuryUrl: import.meta.env.VITE_mercuryUrl,
   // mercuryJwt: import.meta.env.VITE_mercuryJwt,
 });
